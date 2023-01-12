9b76ceb65f8f34d8fe1d3bd1ef66c59729f6566b
jmtj <jmtj.johnson@gmail.com>
2023-01-12 16:06:24 -0500

    fix collapsible bars

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("collactive");
+    this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
