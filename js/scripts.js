32f002f507dfd104ef974b73d2adaa26be3864e9
jmtj <jmtj.johnson@gmail.com>
2023-01-17 11:21:47 -0500

    Update title for pages

--- js/scripts.js
+++ js/scripts.js
@@ -56,16 +56,16 @@ function showSlides(n) {
 }
 
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let topbutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    topbutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    topbutton.style.display = "none";
   }
 }
 
